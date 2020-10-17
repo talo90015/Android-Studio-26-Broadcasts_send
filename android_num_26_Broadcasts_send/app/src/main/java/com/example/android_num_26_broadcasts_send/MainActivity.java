969dd855f3e08127c4f3a14f7dba0a97503a28cf
PotatoServer <talo90015@gmail.com>
2020-10-17 17:07:38 +0800

    Broadcasts_send

---
+++ android_num_26_Broadcasts_send/app/src/main/java/com/example/android_num_26_broadcasts_send/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.android_num_26_broadcasts_send;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public void sand_Box(View v){
+        Intent intent = new Intent();
+        intent.setAction("com.example.android_num_26_Broadcasts_Receive.android_num_26_Broadcasts_send");
+        intent.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
+        sendBroadcast(intent);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
